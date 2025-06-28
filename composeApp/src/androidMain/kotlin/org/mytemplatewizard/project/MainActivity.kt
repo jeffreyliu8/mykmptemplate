388b4222df953d499647f99968834fd855f80cfc
Jeffrey Liu <jeffreyliu8@gmail.com>
2025-06-27 21:10:42 -0700

    refactor: restructure App composable and add theming support

--- composeApp/src/androidMain/kotlin/org/mytemplatewizard/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/mytemplatewizard/project/MainActivity.kt
@@ -11,14 +11,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             App()
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun AppAndroidPreview() {
     App()
