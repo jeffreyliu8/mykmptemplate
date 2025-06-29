caf9aaf5180dd8aac423a3f036d3bb280ef6a077
Jeffrey Liu <jeffreyliu8@gmail.com>
2025-06-28 21:55:04 -0700

    feat: add ProGuard rules for Koin and configure release build settings

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -109,8 +109,15 @@ android {
         }
     }
     buildTypes {
-        getByName("release") {
+//        getByName("release") {
+//            isMinifyEnabled = false
+//        }
+        release {
             isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
