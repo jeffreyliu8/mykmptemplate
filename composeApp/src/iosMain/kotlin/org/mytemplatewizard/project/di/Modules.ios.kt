c79768d07afb56314e31ecb0d9e25744241546af
Jeffrey Liu <jeffreyliu8@gmail.com>
2025-07-21 21:17:21 -0700

    feat: integrate multiplatform-settings for persistent storage
    This commit introduces the `multiplatform-settings` library to enable persistent data storage across Android, iOS, Desktop, and WasmJs platforms.
    
    Key changes:
    - Added `com.russhwolf:multiplatform-settings:1.3.0` dependency to `commonMain`.
    - Added `androidx.preference:preference:1.2.0` dependency to `androidMain` for SharedPreferences integration.
    - Implemented platform-specific `Settings` providers in `Modules.android.kt`, `Modules.ios.kt`, `Modules.desktop.kt`, and `Modules.wasmJs.kt` using `SharedPreferencesSettings`, `NSUserDefaultsSettings`, `PreferencesSettings`, and `StorageSettings` respectively.
    - Updated `SampleRepository` and `SampleRepositoryImpl` to include methods for setting and getting string values using the injected `Settings` instance.
    - Modified `HomePaneViewModel` to demonstrate saving a value to settings when `sampleInt` exceeds 10 and printing the retrieved setting.
    - Uncommented and enabled `platformModule` in `InitKoin.kt` to include platform-specific settings bindings in Koin.
    - Injected `Settings` into `SampleRepositoryImpl` via its constructor.

---
+++ composeApp/src/iosMain/kotlin/org/mytemplatewizard/project/di/Modules.ios.kt
@@ -0,0 +1,13 @@
+package org.mytemplatewizard.project.di
+
+import com.russhwolf.settings.NSUserDefaultsSettings
+import com.russhwolf.settings.Settings
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import platform.Foundation.NSUserDefaults
+
+actual val platformModule: Module = module {
+    single<Settings> {
+        NSUserDefaultsSettings(NSUserDefaults.standardUserDefaults)
+    }
+}
---
+++ composeApp/src/wasmJsMain/kotlin/org/mytemplatewizard/project/di/Modules.wasmJs.kt
@@ -0,0 +1,12 @@
+package org.mytemplatewizard.project.di
+
+import com.russhwolf.settings.Settings
+import com.russhwolf.settings.StorageSettings
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual val platformModule: Module = module {
+    single<Settings> {
+        StorageSettings()
+    }
+}
