72131e18e211ebcf568018eb370bef639fdd30fa
Jeffrey Liu <jeffreyliu8@gmail.com>
2025-11-22 12:26:23 -0800

    feat: refactor project structure and add temporary workaround for Kotlin issue

--- composeApp/src/webMain/kotlin/org/mytemplatewizard/project/Main.kt
+++ composeApp/src/webMain/kotlin/org/mytemplatewizard/project/Main.kt
@@ -2,13 +2,12 @@ package org.mytemplatewizard.project
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
-import kotlinx.browser.document
 import org.mytemplatewizard.project.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     initKoin()
-    ComposeViewport(document.body!!) {
+    ComposeViewport {
         App()
     }
 }
